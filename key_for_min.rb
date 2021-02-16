df70268c69a74ea9e0f1911f64aff54efd5736f1
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-16 16:41:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   
-   name_hash.each do |name_hash, value|
+   name_hash.each do |name_hash, index|
 
-puts "#{name_hash+1}. #{value}"
+puts "#{name_hash+1}. #{index}"
 
 
 end
